aa9c9733f04d19aaf80be3252c3b5a4251213f92
Alban Daumer <daumera@gmail.com>
2024-01-02 17:53:56 +0100

    link the dumb controller to service

--- src/main/java/daumer/alban/alten/shop/back/products/controller/ProductController.java
+++ src/main/java/daumer/alban/alten/shop/back/products/controller/ProductController.java
@@ -1,20 +1,23 @@
 package daumer.alban.alten.shop.back.products.controller;
 
 import daumer.alban.alten.shop.back.products.bean.ProductBean;
+import daumer.alban.alten.shop.back.products.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ProductBean> getProducts() {
-        return Collections.emptyList();
-
+        return productService.getAll();
     }
 
 }
