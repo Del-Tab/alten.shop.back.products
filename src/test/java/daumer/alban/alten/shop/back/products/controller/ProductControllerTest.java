ac0f6ae4d6635c67ca7df4129623c8658ff53e85
Alban Daumer <daumera@gmail.com>
2024-01-03 11:47:56 +0100

    T+5h15 (+15minutes): a parameterized test to ensure all data json parameters match expected values

--- src/test/java/daumer/alban/alten/shop/back/products/controller/ProductControllerTest.java
+++ src/test/java/daumer/alban/alten/shop/back/products/controller/ProductControllerTest.java
@@ -5,10 +5,13 @@ import daumer.alban.alten.shop.back.products.enums.InventoryStatusEnum;
 import daumer.alban.alten.shop.back.products.model.Product;
 import daumer.alban.alten.shop.back.products.repository.ProductRepository;
 import jakarta.servlet.ServletContext;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,14 +23,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
@@ -42,12 +47,12 @@ class ProductControllerTest {
     public static final String CODE = "code";
     public static final String NAME = "name";
     public static final String DESCRIPTION = "description";
-    public static final long PRICE = 2L;
-    public static final long QUANTITY = 3L;
+    public static final Long PRICE = 2L;
+    public static final Long QUANTITY = 3L;
     public static final InventoryStatusEnum INVENTORY_STATUS_ENUM = InventoryStatusEnum.INSTOCK;
     public static final CategoryEnum CATEGORY_ENUM = CategoryEnum.Accessories;
     public static final String IMAGE = "image";
-    public static final long RATING = 4L;
+    public static final Long RATING = 4L;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -77,12 +82,13 @@ class ProductControllerTest {
 
     @Test
     public void givenNoProduct_whenRequestList_thenReturnsEmptyArrayJson() throws Exception {
+        //nb this test is also covered by givenNProducts_whenRequestList_thenReturnsNSizedItemArrayJson(0)
         when(productRepository.findAll()).thenReturn(Collections.emptyList());
 
         MvcResult mvcResult = mockMvc
                 .perform(MockMvcRequestBuilders.get("/products")
                         .contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
-                .andDo(MockMvcResultHandlers.print())
+                //.andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty())
                 .andReturn();
@@ -104,7 +110,7 @@ class ProductControllerTest {
         MvcResult mvcResult = mockMvc
                 .perform(MockMvcRequestBuilders.get("/products")
                         .contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
-                .andDo(MockMvcResultHandlers.print())
+                //.andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(ID))
@@ -117,6 +123,39 @@ class ProductControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].category").value(CATEGORY_ENUM.name()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].image").value(IMAGE))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].rating").value(RATING))
+                .andReturn();
+
+        assertEquals("application/json", mvcResult.getResponse().getContentType());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0", "1", "2", "5", "26", "42042"})
+    public void givenNProducts_whenRequestList_thenReturnsNSizedItemArrayJson(int nbElements) throws Exception {
+        when(productRepository.findAll()).thenReturn(Stream.generate(() -> new Product(
+                ID, CODE,
+                NAME,
+                DESCRIPTION,
+                PRICE,
+                QUANTITY,
+                INVENTORY_STATUS_ENUM,
+                CATEGORY_ENUM, IMAGE, RATING)).limit(nbElements).collect(Collectors.toList()));
+
+        MvcResult mvcResult = mockMvc
+                .perform(MockMvcRequestBuilders.get("/products")
+                        .contentType(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
+                //.andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(nbElements)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].id", Matchers.everyItem(equalTo(ID.intValue()))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].code", Matchers.everyItem(equalTo(CODE))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].name", Matchers.everyItem(equalTo(NAME))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].description", Matchers.everyItem(equalTo(DESCRIPTION))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].price", Matchers.everyItem(equalTo(PRICE.intValue()))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].quantity", Matchers.everyItem(equalTo(QUANTITY.intValue()))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].inventoryStatus", Matchers.everyItem(equalTo(INVENTORY_STATUS_ENUM.name()))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].category", Matchers.everyItem(equalTo(CATEGORY_ENUM.name()))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].image", Matchers.everyItem(equalTo(IMAGE))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[*].rating", Matchers.everyItem(equalTo(RATING.intValue()))))
                 .andReturn();
 
         assertEquals("application/json", mvcResult.getResponse().getContentType());
